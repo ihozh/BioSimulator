4af637f610fd141eccbbc8baa7854547b050d86e
ihozh <1292496376@qq.com>
2015-11-11 02:40:34 -0500

    Update first_cuda.cu

--- first_cuda.cu
+++ first_cuda.cu
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <cuda_runtime.h>
+
+#define DATA_SIZE 1048576
+
+int data[DATA_SIZE];
+
+__global__ static void sumOfSquares(int *num, int* result)
+{
+    int sum = 0;
+    int i;
+    for(i = 0; i < DATA_SIZE; i++) {
+        sum += num[i] * num[i];
+    }
+
+    *result = sum;
+}
+void GenerateNumbers(int *number, int size)
+{
+    for(int i = 0; i < size; i++) {
+        number[i] = rand() % 10;
+    }
+}
 bool InitCUDA()
 {
     int count;
@@ -37,6 +59,21 @@ int main()
     }
 
     printf("CUDA initialized.\n");
+    
+    GenerateNumbers(data, DATA_SIZE);
+    int* gpudata, *result;
+    cudaMalloc((void**) &gpudata, sizeof(int) * DATA_SIZE);
+    cudaMalloc((void**) &result, sizeof(int));
+    cudaMemcpy(gpudata, data, sizeof(int) * DATA_SIZE,cudaMemcpyHostToDevice);
+    
+    sumOfSquares<<<1, 1, 0>>>(gpudata, result);
+
+    int sum;
+    cudaMemcpy(&sum, result, sizeof(int), cudaMemcpyDeviceToHost);
+    cudaFree(gpudata);
+    cudaFree(result);
+
+    printf("sum: %d\n", sum);
 
     return 0;
 }
